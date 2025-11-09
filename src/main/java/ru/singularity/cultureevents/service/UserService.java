4db84cb42bbdf229555d5cedea2b3dda5aaf2be7
lonelydread <fabrikint2005@gmail.com>
2025-11-09 18:25:22 +0300

    Удаление логики о пользователе
    Логика данных о пользователе будет реализована на стороне сайта в js файле. Благодаря Max Bridge будем хранить информацию о пользователе и его предпочтениях на локальном хранилище и будем передавать эти данные на бекенд

--- src/main/java/ru/singularity/cultureevents/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package ru.singularity.cultureevents.service;
-
-import ru.singularity.cultureevents.model.UserPreferences;
-
-public interface UserService {
-    UserPreferences getUserPreferences(String userId);
-    UserPreferences saveUserPreferences(String userId, UserPreferences preferences);
-}
