d52241503c559efa33cc8f91e01f45b93324903a
lonelydread <fabrikint2005@gmail.com>
2025-11-09 18:26:00 +0300

    Добавление функции рекомендаций без реализации

--- src/main/java/ru/singularity/cultureevents/controller/EventController.java
+++ src/main/java/ru/singularity/cultureevents/controller/EventController.java
@@ -3,16 +3,25 @@ package ru.singularity.cultureevents.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.singularity.cultureevents.dto.RecommendationRequest;
 import ru.singularity.cultureevents.model.Event;
 import ru.singularity.cultureevents.service.EventService;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class EventController {
 
     private final EventService eventService;
+
+    // Персонализированные рекомендации
+    @PostMapping("/recommendations")
+    public List<Event> getPersonalizedRecommendations(@RequestBody RecommendationRequest request) {
+        return eventService.getPersonalizedRecommendations(request);
+    }
 
     @GetMapping
     public List<Event> getAllEvents(@RequestParam(required = false) String city) {
--- src/main/java/ru/singularity/cultureevents/service/EventService.java
+++ src/main/java/ru/singularity/cultureevents/service/EventService.java
@@ -1,9 +1,12 @@
 package ru.singularity.cultureevents.service;
 
+import ru.singularity.cultureevents.dto.RecommendationRequest;
 import ru.singularity.cultureevents.model.Event;
+
 import java.util.List;
 
 public interface EventService {
+    List<Event> getPersonalizedRecommendations(RecommendationRequest request);
     List<Event> getAllEvents();
     List<Event> getEventsByCity(String city);
     List<Event> getEventsByCategory(String category);
