27169a3f6b006a4fb642306b8ca6367944f2f732
lonelydread <fabrikint2005@gmail.com>
2025-11-09 17:40:47 +0300

    Создание контроллера API запросов мероприятий

---
+++ src/main/java/ru/singularity/cultureevents/controller/EventController.java
@@ -0,0 +1,46 @@
+package ru.singularity.cultureevents.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import ru.singularity.cultureevents.model.Event;
+import ru.singularity.cultureevents.service.EventService;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public List<Event> getAllEvents(@RequestParam(required = false) String city) {
+        // TODO: реализовать
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public Event getEventById(@PathVariable String id) {
+        // TODO: реализовать
+        return null;
+    }
+
+    @GetMapping("/recommendations")
+    public List<Event> getRecommendedEvents(
+            @RequestParam String city,
+            @RequestParam(required = false) String mood,
+            @RequestParam String userId) {
+        // TODO: реализовать
+        return null;
+    }
+
+    @GetMapping("/search")
+    public List<Event> searchEvents(
+            @RequestParam String city,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String query) {
+        // TODO: реализовать
+        return null;
+    }
+}
