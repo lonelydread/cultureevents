e0bd0db243fd6d2e677b53ba88d4fda6e4e971bd
lonelydread <fabrikint2005@gmail.com>
2025-11-09 17:38:48 +0300

    Создание сервиса мероприятий

--- src/main/java/ru/singularity/cultureevents/model/Event.java
+++ src/main/java/ru/singularity/cultureevents/model/Event.java
@@ -1,5 +1,8 @@
 package ru.singularity.cultureevents.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.Data;
 
 @Data
 @Document(collection = "events")
