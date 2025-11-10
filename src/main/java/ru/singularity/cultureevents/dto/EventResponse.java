642bfddfd8d74b1d8cf9ee558d75892f74e10d43
lonelydread <fabrikint2005@gmail.com>
2025-11-10 15:26:57 +0300

    Добавление сущности ответа

---
+++ src/main/java/ru/singularity/cultureevents/dto/EventResponse.java
@@ -0,0 +1,21 @@
+package ru.singularity.cultureevents.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+public class EventResponse {
+    private Integer id;
+    private String title;
+    private String description;
+    private String category;
+    private String city;
+    private LocalDateTime date;
+    private String location;
+    private BigDecimal price;
+    private String imageUrl;
+    private String mood;
+    private String audience;
+}
