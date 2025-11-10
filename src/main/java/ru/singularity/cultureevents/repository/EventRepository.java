c51fecf4c17c4ae19055eadb3b75476de45f5615
lonelydread <fabrikint2005@gmail.com>
2025-11-10 15:25:56 +0300

    Update EventRepository.java
    Изменение функциональности

--- src/main/java/ru/singularity/cultureevents/repository/EventRepository.java
+++ src/main/java/ru/singularity/cultureevents/repository/EventRepository.java
@@ -1,16 +1,23 @@
 package ru.singularity.cultureevents.repository;
 
 
-import ru.singularity.cultureevents.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ru.singularity.cultureevents.model.Event;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Repository
-public interface EventRepository extends MongoRepository<Event, String> {
-    List<Event> findByCity(String city);
-    List<Event> findByCategory(String category);
-    List<Event> findByCityAndCategory(String city, String category);
-    List<Event> findByMood(String mood);
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findByCityAndDateAfter(String city, LocalDateTime date);
+//    List<Event> findByCityAndCategoryAndDateAfter(
+//            String city, String category, LocalDateTime date);
+//    List<Event> findByCategoryAndDateAfter(String category, LocalDateTime date);
+//    List<Event> findByCityAndMoodAndDateAfter(
+//            String city, String mood, LocalDateTime date);
+//    List<Event> findByCity(String city);
+//    List<Event> findByDateAfter(LocalDateTime date);
+
 }
