9aac47315f9e4465bf5c295d2554e5ba5b372945
lonelydread <fabrikint2005@gmail.com>
2025-11-10 15:27:07 +0300

    Изменение контроллера

--- src/main/java/ru/singularity/cultureevents/controller/EventController.java
+++ src/main/java/ru/singularity/cultureevents/controller/EventController.java
@@ -2,54 +2,37 @@ package ru.singularity.cultureevents.controller;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.singularity.cultureevents.dto.EventResponse;
 import ru.singularity.cultureevents.dto.RecommendationRequest;
+import ru.singularity.cultureevents.mapper.EventMapper;
 import ru.singularity.cultureevents.model.Event;
 import ru.singularity.cultureevents.service.EventService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*")
 public class EventController {
 
     private final EventService eventService;
+    private final EventMapper eventMapper;
 
-    // Персонализированные рекомендации
     @PostMapping("/recommendations")
-    public List<Event> getPersonalizedRecommendations(@RequestBody RecommendationRequest request) {
-        return eventService.getPersonalizedRecommendations(request);
-    }
+    public ResponseEntity<List<EventResponse>> getPersonalizedRecommendations(
+            @RequestBody RecommendationRequest request) {
 
-    @GetMapping
-    public List<Event> getAllEvents(@RequestParam(required = false) String city) {
-        // TODO: реализовать
-        return null;
-    }
+        List<Event> events = eventService.getPersonalizedRecommendations(request);
+        List<EventResponse> response = events.stream()
+                .map(eventMapper::toResponse)
+                .collect(Collectors.toList());
 
-    @GetMapping("/{id}")
-    public Event getEventById(@PathVariable String id) {
-        // TODO: реализовать
-        return null;
-    }
-
-    @GetMapping("/recommendations")
-    public List<Event> getRecommendedEvents(
-            @RequestParam String city,
-            @RequestParam(required = false) String mood,
-            @RequestParam String userId) {
-        // TODO: реализовать
-        return null;
-    }
-
-    @GetMapping("/search")
-    public List<Event> searchEvents(
-            @RequestParam String city,
-            @RequestParam(required = false) String category,
-            @RequestParam(required = false) String query) {
-        // TODO: реализовать
-        return null;
+        return ResponseEntity.ok(response);
     }
 }
