92aa74f00e2d78e470519a75f6f87debdeb8e6e5
itsap2ch <alisapavlova2005@gmail.com>
2025-11-11 23:55:28 +0300

    Свайп-тест и рекомендации
    Card - структура карточки для теста;
    Recommendation - рекомендации в ленте с учетом погоды и настроения;
    SwipeProcessor - обновление предпочтений пользователей после теста;
    swipe_test_cards - набор карточек для теста на всякий;

---
+++ src/main/java/ru/singularity/cultureevents/model/Card.java
@@ -0,0 +1,10 @@
+package ru.singularity.cultureevents.model;
+import java.util.Map;
+
+public class Card {
+    public int id;
+    public String title;
+    public String description;
+    public String image_url;
+    public Map<String, Integer> tags;
+}
